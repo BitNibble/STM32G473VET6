6e23c6486e31042da1b6c839463c7fb84cbbaee8
Sergio Santos <sergi@Victus>
2026-06-06 15:06:04 +0100

    st7789_trial_1

--- STM32G473VET6_1/Src/main.c
+++ STM32G473VET6_1/Src/main.c
@@ -72,13 +72,28 @@ int main(void)
 
 	ST7789 lcd1 = st7789_enable(dev()->comm->spi3, 7, 8, 9, NULL);
 	(void) lcd1;
+
+	lcd1.start(&lcd1.par);
+	lcd1.draw_circle(&lcd1.par,200,80,15,ST77XX_BLACK);
+	lcd1.draw_star5(&lcd1.par,200,80,15,5,ST77XX_GOLD);
+	lcd1.stop(&lcd1.par);
+
 	//tim1_blink_setup();
+
 
 	while(1)
 	{
 		set_pin( dev()->gpio->f, 2 );
+		lcd1.start(&lcd1.par);
+		lcd1.drawstring16x24_size(&lcd1.par,"LED ON",10,20,ST77XX_MAGENTA,ST77XX_GREEN, 8);
+		lcd1.stop(&lcd1.par);
+
 		_delay_ms(3000);
 		clear_pin( dev()->gpio->f, 2 );
+		lcd1.start(&lcd1.par);
+		lcd1.drawstring16x24_size(&lcd1.par,"LED OFF",10,20,ST77XX_MAGENTA,ST77XX_GREEN, 8);
+		lcd1.stop(&lcd1.par);
+
 		_delay_ms(3000);
 	}
 }
