dfb58a0d5a6f33e0bbc655dd4619290e68c0c013
Sergio Santos <sergi@Victus>
2026-07-09 16:26:39 +0100

    i2c1 default par

--- STM32G473VET6_3/lib/src/stm32gxxx_i2c1.c
+++ STM32G473VET6_3/lib/src/stm32gxxx_i2c1.c
@@ -1,119 +1,22 @@
 #include "stm32gxxx_i2c1.h"
 
 /* --- Private Variables --- */
-static i2c1_par i2c1_internal_par = {0};
-static i2c1_irq i2c1_internal_irq = {0};
+static i2c1_par i2c1_internal_par = { // DEFAULT
+	.scl_port = GPIOB,
+	.sda_port = GPIOB,
+	.scl_pin = 8,
+	.sda_pin = 9,
+	.alternate_function = 4,
+	.timing = DEFAULT_BUS_TIMING,
+	.own_address = 'A',
+	.addressing_mode = I2C1_ADDR_7BIT,
+	.analog_filter = I2C1_FILTER_DISABLE,
+	.digital_filter = I2C1_FILTER_DISABLE,
+	.status = ZERO
+};
 
-/* --- Private Function Forward Declarations --- */
-static uint8_t get_busy(void);
-static uint8_t get_ready(void);
-static uint8_t get_enabled(void);
-static uint8_t get_error(void);
-
-static void set_timing(uint32_t timing);
-static void set_own_address(uint16_t address);
-static void set_addressing_mode(uint8_t mode);
-static void set_analog_filter(uint8_t state);
-static void set_digital_filter(uint8_t filter_value);
-
-/* Sub-interface: Common */
-static void _common_config(void);
-static void common_init(void);
-static void common_enable(void);
-static void common_disable(void);
-static void common_reset(void);
 static void common_irq_event(void);
 static void common_irq_error(void);
-
-/* Sub-interface: Master */
-static int8_t master_write_byte(uint16_t address, uint8_t data);
-static int8_t master_read_byte(uint16_t address, uint8_t *data);
-static int8_t master_write(uint16_t address, const uint8_t *buffer, uint16_t length);
-static int8_t master_read(uint16_t address, uint8_t *buffer, uint16_t length);
-static int8_t master_probe(uint16_t address);
-static int8_t master_scan(uint8_t *list, uint16_t *count);
-
-/* Sub-interface: Memory */
-static int8_t memory_write(uint16_t address, uint16_t reg, const uint8_t *buffer, uint16_t length);
-static int8_t memory_read(uint16_t address, uint16_t reg, uint8_t *buffer, uint16_t length);
-
-/* Sub-interface: Slave */
-static void slave_enable(void);
-static void slave_disable(void);
-static void slave_listen(void);
-
-/* Sub-interface: DMA */
-static int8_t dma_write(uint16_t address, const uint8_t *buffer, uint16_t length);
-static int8_t dma_read(uint16_t address, uint8_t *buffer, uint16_t length);
-
-/* --- Object Interface Structures --- */
-static i2c1_get i2c1_internal_get = {
-    .busy    = get_busy,
-    .ready   = get_ready,
-    .enabled = get_enabled,
-    .error   = get_error
-};
-
-static i2c1_set i2c1_internal_set = {
-    .timing          = set_timing,
-    .own_address     = set_own_address,
-    .addressing_mode = set_addressing_mode,
-    .analog_filter   = set_analog_filter,
-    .digital_filter  = set_digital_filter
-};
-
-static const i2c1_common i2c1_execute_common = {
-    .init      = common_init,
-    .enable    = common_enable,
-    .disable   = common_disable,
-    .reset     = common_reset,
-    .irq_event = common_irq_event,
-    .irq_error = common_irq_error
-};
-
-static const i2c1_master i2c1_execute_master = {
-    .write_byte = master_write_byte,
-    .read_byte  = master_read_byte,
-    .write      = master_write,
-    .read       = master_read,
-    .probe      = master_probe,
-    .scan       = master_scan
-};
-
-static const i2c1_memory i2c1_execute_memory = {
-    .write = memory_write,
-    .read  = memory_read
-};
-
-static const i2c1_slave i2c1_execute_slave = {
-    .enable  = slave_enable,
-    .disable = slave_disable,
-    .listen  = slave_listen
-};
-
-static const i2c1_dma i2c1_execute_dma = {
-    .write = dma_write,
-    .read  = dma_read
-};
-
-static const i2c1_run i2c1_execute = {
-    .common = &i2c1_execute_common,
-    .master = &i2c1_execute_master,
-    .memory = &i2c1_execute_memory,
-    .slave  = &i2c1_execute_slave,
-    .dma    = &i2c1_execute_dma
-};
-
-static i2c1_handler i2c1_obj = {
-    .par     = &i2c1_internal_par,
-    .get     = &i2c1_internal_get,
-    .set     = &i2c1_internal_set,
-    .irq     = &i2c1_internal_irq,
-    .run     = &i2c1_execute
-};
-
-/* --- Public Interface Accessor --- */
-i2c1_handler *i2c1(void) { return &i2c1_obj; }
 
 /* --- GET Functions Implementation --- */
 static uint8_t get_busy(void) 
@@ -278,49 +181,7 @@ static void common_reset(void)
     common_init();
 }
 
-static void common_irq_event(void) 
-{
-    uint32_t isr = I2C1->ISR;
-    
-    if ((isr & I2C_ISR_TC) && (I2C1->CR1 & I2C_CR1_TCIE)) 
-    {
-        if (i2c1_internal_irq.tx_complete) i2c1_internal_irq.tx_complete();
-    }
-    if ((isr & I2C_ISR_RXNE) && (I2C1->CR1 & I2C_CR1_RXIE)) 
-    {
-        if (i2c1_internal_irq.rx_complete) i2c1_internal_irq.rx_complete();
-    }
-    if ((isr & I2C_ISR_ADDR) && (I2C1->CR1 & I2C_CR1_ADDRIE)) 
-    {
-        I2C1->ICR = I2C_ICR_ADDRCF;
-        if (i2c1_internal_irq.listen) i2c1_internal_irq.listen();
-    }
-}
-
-static void common_irq_error(void) 
-{
-    uint32_t isr = I2C1->ISR;
-    i2c1_internal_par.status = isr;
-    
-    if (isr & (I2C_ISR_BERR | I2C_ISR_ARLO | I2C_ISR_OVR | I2C_ISR_TIMEOUT)) 
-    {
-        /* Fixed: ST CMSIS uses I2C_ICR_TIMOUTCF (no 'E') */
-        I2C1->ICR = (I2C_ICR_BERRCF | I2C_ICR_ARLOCF | I2C_ICR_OVRCF | I2C_ICR_TIMOUTCF);
-        if (i2c1_internal_irq.error) i2c1_internal_irq.error();
-    }
-}
-
 /* --- MASTER Execution Functions --- */
-static int8_t master_write_byte(uint16_t address, uint8_t data) 
-{
-    return master_write(address, &data, 1);
-}
-
-static int8_t master_read_byte(uint16_t address, uint8_t *data) 
-{
-    return master_read(address, data, 1);
-}
-
 static int8_t master_write(uint16_t address, const uint8_t *buffer, uint16_t length) 
 {
     if (buffer == NULL || length == 0) return -1;
@@ -404,6 +265,16 @@ static int8_t master_read(uint16_t address, uint8_t *buffer, uint16_t length)
     return 0;
 }
 
+static int8_t master_write_byte(uint16_t address, uint8_t data)
+{
+    return master_write(address, &data, 1);
+}
+
+static int8_t master_read_byte(uint16_t address, uint8_t *data)
+{
+    return master_read(address, data, 1);
+}
+
 static int8_t master_probe(uint16_t address) 
 {
     uint32_t timeout = 100000U;
@@ -431,6 +302,7 @@ static int8_t master_probe(uint16_t address)
     I2C1->ICR = I2C_ICR_STOPCF;
     return 0;
 }
+
 static int8_t master_scan(uint8_t *list, uint16_t *count){
 	if (list == NULL || count == NULL)
 		return -1;
@@ -444,6 +316,7 @@ static int8_t master_scan(uint8_t *list, uint16_t *count){
 	}
 	*count = found_devices;return 0;
 }
+
 /* --- MEMORY Execution Functions --- */
 static int8_t memory_write(uint16_t address, uint16_t reg, const uint8_t *buffer, uint16_t length){
 	if (buffer == NULL || length == 0)
@@ -484,6 +357,7 @@ static int8_t memory_write(uint16_t address, uint16_t reg, const uint8_t *buffer
 	I2C1->ICR = I2C_ICR_STOPCF;
 	return 0;
 }
+
 static int8_t memory_read(uint16_t address, uint16_t reg, uint8_t *buffer, uint16_t length){
 	if (buffer == NULL || length == 0)
 		return -1;
@@ -533,16 +407,20 @@ static int8_t memory_read(uint16_t address, uint16_t reg, uint8_t *buffer, uint1
 	I2C1->ICR = I2C_ICR_STOPCF;
 	return 0;
 }
+
 /* --- SLAVE Execution Functions --- */
 static void slave_enable(void){
 	I2C1->CR1 |= (I2C_CR1_ADDRIE | I2C_CR1_RXIE | I2C_CR1_NACKIE);
 }
+
 static void slave_disable(void){
 	I2C1->CR1 &= ~(I2C_CR1_ADDRIE | I2C_CR1_RXIE | I2C_CR1_NACKIE);
 }
+
 static void slave_listen(void){
 	I2C1->ICR = I2C_ICR_ADDRCF;
 }
+
 /* --- DMA Execution Functions --- */
 static int8_t dma_write(uint16_t address, const uint8_t *buffer, uint16_t length){
 	if (buffer == NULL || length == 0)
@@ -555,6 +433,7 @@ static int8_t dma_write(uint16_t address, const uint8_t *buffer, uint16_t length
 	cr2 |= I2C_CR2_START;I2C1->CR2 = cr2;
 	return 0;
 }
+
 static int8_t dma_read(uint16_t address, uint8_t *buffer, uint16_t length){
 	if (buffer == NULL || length == 0)
 		return -1;
@@ -565,5 +444,108 @@ static int8_t dma_read(uint16_t address, uint8_t *buffer, uint16_t length){
 	cr2 |= I2C_CR2_RD_WRN;cr2 |= I2C_CR2_AUTOEND;
 	cr2 |= I2C_CR2_START;I2C1->CR2 = cr2;
 	return 0;
+}
+
+static i2c1_irq i2c1_internal_irq = {0};
+
+/* --- Object Interface Structures --- */
+static i2c1_get i2c1_internal_get = {
+    .busy    = get_busy,
+    .ready   = get_ready,
+    .enabled = get_enabled,
+    .error   = get_error
+};
+
+static i2c1_set i2c1_internal_set = {
+    .timing          = set_timing,
+    .own_address     = set_own_address,
+    .addressing_mode = set_addressing_mode,
+    .analog_filter   = set_analog_filter,
+    .digital_filter  = set_digital_filter
+};
+
+static const i2c1_common i2c1_execute_common = {
+    .init      = common_init,
+    .enable    = common_enable,
+    .disable   = common_disable,
+    .reset     = common_reset,
+    .irq_event = common_irq_event,
+    .irq_error = common_irq_error
+};
+
+static const i2c1_master i2c1_execute_master = {
+    .write_byte = master_write_byte,
+    .read_byte  = master_read_byte,
+    .write      = master_write,
+    .read       = master_read,
+    .probe      = master_probe,
+    .scan       = master_scan
+};
+
+static const i2c1_memory i2c1_execute_memory = {
+    .write = memory_write,
+    .read  = memory_read
+};
+
+static const i2c1_slave i2c1_execute_slave = {
+    .enable  = slave_enable,
+    .disable = slave_disable,
+    .listen  = slave_listen
+};
+
+static const i2c1_dma i2c1_execute_dma = {
+    .write = dma_write,
+    .read  = dma_read
+};
+
+static const i2c1_run i2c1_execute = {
+    .common = &i2c1_execute_common,
+    .master = &i2c1_execute_master,
+    .memory = &i2c1_execute_memory,
+    .slave  = &i2c1_execute_slave,
+    .dma    = &i2c1_execute_dma
+};
+
+static i2c1_handler i2c1_obj = {
+    .par     = &i2c1_internal_par,
+    .get     = &i2c1_internal_get,
+    .set     = &i2c1_internal_set,
+    .irq     = &i2c1_internal_irq,
+    .run     = &i2c1_execute
+};
+
+/* --- Public Interface Accessor --- */
+i2c1_handler *i2c1(void) { return &i2c1_obj; }
+
+static void common_irq_event(void)
+{
+    uint32_t isr = I2C1->ISR;
+
+    if ((isr & I2C_ISR_TC) && (I2C1->CR1 & I2C_CR1_TCIE))
+    {
+        if (i2c1_internal_irq.tx_complete) i2c1_internal_irq.tx_complete();
+    }
+    if ((isr & I2C_ISR_RXNE) && (I2C1->CR1 & I2C_CR1_RXIE))
+    {
+        if (i2c1_internal_irq.rx_complete) i2c1_internal_irq.rx_complete();
+    }
+    if ((isr & I2C_ISR_ADDR) && (I2C1->CR1 & I2C_CR1_ADDRIE))
+    {
+        I2C1->ICR = I2C_ICR_ADDRCF;
+        if (i2c1_internal_irq.listen) i2c1_internal_irq.listen();
+    }
+}
+
+static void common_irq_error(void)
+{
+    uint32_t isr = I2C1->ISR;
+    i2c1_internal_par.status = isr;
+
+    if (isr & (I2C_ISR_BERR | I2C_ISR_ARLO | I2C_ISR_OVR | I2C_ISR_TIMEOUT))
+    {
+        /* Fixed: ST CMSIS uses I2C_ICR_TIMOUTCF (no 'E') */
+        I2C1->ICR = (I2C_ICR_BERRCF | I2C_ICR_ARLOCF | I2C_ICR_OVRCF | I2C_ICR_TIMOUTCF);
+        if (i2c1_internal_irq.error) i2c1_internal_irq.error();
+    }
 }
 
